ec1d2d19cb948027306f2a5a2467ee8062ae4ce3
Leonardo Quaddro <leonardodisouza@hotmail.com>
2017-08-11 20:52:17 -0300

    implementando servidorzinho na unha

---
+++ hello.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const index = require('./index.js');
+
+const hostname = '127.0.0.1';
+const port = 8080;
+
+const server = http.createServer(function(req, res) {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end(index);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = `<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Desenvolvido com NodeJS</title>
+  </head>
+  <body>
+    <h1>Servidor na Unha!</h1>
+    <h2>Leonardo Souza</h2>
+  </body>
+</html>`;
