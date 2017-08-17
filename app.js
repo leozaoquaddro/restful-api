2848ce3e209b40a9800cbf990ba3b6732db9f4c6
Leonardo Quaddro <leonardodisouza@hotmail.com>
2017-08-16 22:45:08 -0300

    configurando mongoose e implementando gets

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 const PORT = 3000
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 const express = require('express')
 const moment = require('moment')
 const bodyParser = require('body-parser')
 const app = express()
+
+// Mongoose Connection
+mongoose.connect('mongodb://localhost/local', { useMongoClient: true })
+
+// Customer Schema
+const customerSchema = new Schema({
+  name: String,
+  email: String,
+  phone: String,
+  birthDay: Date,
+  weight: Number,
+  height: Number,
+  sex: String,
+  rich: Boolean,
+  createAt: { type: Date, default: Date.now } 
+})
+
+// Customer Model (Interface)
+const CostumerModel = mongoose.model('costumers', customerSchema)
 
 // Middlewares
 // Custom CurrentTime
@@ -18,9 +39,11 @@ const myLogger = function(req, res, next) {
   next()
 }
 
+// Register Middlewares
 app.use(requestCurrentTime)
 app.use(myLogger)
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // Routes
 // GET /
@@ -41,49 +64,37 @@ app.get('/', (req, res) => {
 
 // GET /costumers
 app.get('/costumers', (req, res) => {
-  console.log(req.query)
-  const costumers = [
-    {
-      id: 1,
-      name: "John Doe",
-      email: "john@doe.com"
-    },
-    {
-      id: 2,
-      name: "Mary Doe",
-      email: "mary@doe.com"
-    },
-    {
-      id: 3,
-      name: "Susan Doe",
-      email: "susan@doe.com"
-    }
-  ]
-  res.send(costumers)
+  CostumerModel.find({}, function(err, costumers) {
+    if (err) res.sendStatus(404)
+    res.status(200).send(costumers)
+  })
 })
 
 // GET /costumers/1
 app.get('/costumers/:id', (req, res) => {
-  console.log(req.params)
-  res.send(req.params.id)
+  // console.log(req.params)
+  CostumerModel.findById(req.params.id, function(err, costumer) {
+    if (err) res.sendStatus(404)
+    res.status(200).send(costumer)
+  })
 })
 
 // POST /costumers
 app.post('/costumers', (req, res) => {
   console.log(req.body)
-  res.send('POST')
+  res.status(201).send('POST')
 })
 
 // PUT /costumers/1
 app.put('/costumers/:id', (req, res) => {
   console.log(req.params)
-  res.send(req.params.id)
+  res.sendStatus(204)
 })
 
 // DELETE /costumers/1
 app.delete('/costumers/:id', (req, res) => {
   console.log(req.params)
-  res.send(req.params.id)
+  res.status(204).end()
 })
 
 
